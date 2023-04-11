c9474c06317f59fbc17f8ef86f4ad4b632ca03f0
will-I-am26 <yawill@umich.edu>
2023-04-11 18:28:15 -0400

    Trying to create the tank class(Not working yet)

--- FinalProject/MainPage.xaml.cs
+++ FinalProject/MainPage.xaml.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.Graphics.Canvas.UI.Xaml;
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Brushes;
+using Microsoft.Graphics.Canvas.UI;
+using Microsoft.Graphics.Canvas.UI.Xaml;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI;
@@ -13,6 +18,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -24,15 +30,78 @@ namespace FinalProject
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            Window.Current.CoreWindow.KeyDown += Canvas_KeyDown;
+            Window.Current.CoreWindow.KeyUp += Canvas_KeyUp;
         }
 
-        void CanvasControl_Draw(CanvasControl sender, CanvasDrawEventArgs args)
+        tankGame tankgame;
+        private CanvasBitmap tankimage;
+
+
+
+        private void Canvas_KeyDown(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.KeyEventArgs e)
         {
-            args.DrawingSession.DrawEllipse(155, 115, 80, 30, Colors.Black, 3);
-            args.DrawingSession.DrawText("Hello, world!", 100, 100, Colors.Yellow);
+            if (e.VirtualKey == Windows.System.VirtualKey.Left)
+            {
+                tankgame.goingLeft(true);
+            }
+            else if (e.VirtualKey == Windows.System.VirtualKey.Right)
+            {
+                tankgame.goingRight(true);
+            }
+            else if (e.VirtualKey == Windows.System.VirtualKey.Up)
+            {
+                tankgame.goingUp(true);
+            }
+            else if (e.VirtualKey == Windows.System.VirtualKey.Down)
+            {
+                tankgame.goingDown(true);
+            }
+        }
+
+        private void Canvas_KeyUp(Windows.UI.Core.CoreWindow sender, Windows.UI.Core.KeyEventArgs e)
+        {
+            if (e.VirtualKey == Windows.System.VirtualKey.Left)
+            {
+                tankgame.goingLeft(false);
+            }
+            else if (e.VirtualKey == Windows.System.VirtualKey.Right)
+            {
+                tankgame.goingRight(false);
+            }
+            else if (e.VirtualKey == Windows.System.VirtualKey.Up)
+            {
+                tankgame.goingUp(false);
+            }
+            else if (e.VirtualKey == Windows.System.VirtualKey.Down)
+            {
+                tankgame.goingDown(false);
+            }
+        }
+
+        private void Canvas_Draw(ICanvasAnimatedControl sender, CanvasAnimatedDrawEventArgs args)
+        {
+            tankgame.DrawGame(args.DrawingSession);
+        }
+
+        private void Canvas_Update(ICanvasAnimatedControl sender, CanvasAnimatedUpdateEventArgs args)
+        {
+            tankgame.Update();
+        }
+        private void Canvas_CreateResources(CanvasAnimatedControl sender, Microsoft.Graphics.Canvas.UI.CanvasCreateResourcesEventArgs args)
+        {
+            args.TrackAsyncAction(CreateResources(sender).AsAsyncAction());
+        }
+
+        async Task CreateResources(Microsoft.Graphics.Canvas.UI.Xaml.CanvasAnimatedControl sender)
+        {
+            tankimage = await CanvasBitmap.LoadAsync(sender, "tank.jpg");
+            tankgame = new tankGame(tankimage);
         }
 
         //This is test how branching and merging work
---
+++ FinalProject/Tank.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.Graphics.Canvas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.Gaming.Input;
+using Windows.UI;
+using Windows.UI.Input;
+using Windows.UI.Input.Inking;
+using Windows.UI.Xaml.Documents;
+
+namespace FinalProject
+{
+    public class Tank : IDrawable
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public int Speed { get; set; }
+
+        private CanvasBitmap image;
+
+        public bool TravelingDownward { get; set; }
+        public bool TravelingLeftward { get; set; }
+
+        public bool TravelingUpward { get; set; }
+        public bool TravelingRightward { get; set; }
+
+        public Tank(int x, int y, int speed, CanvasBitmap image)
+        {
+            X = x;
+            Y = y;
+            this.image = image;
+            Speed = speed;
+            TravelingDownward = false;
+            TravelingLeftward = false;
+            TravelingRightward = false;
+            TravelingUpward= false;
+        }
+
+        public void Update()
+        {
+            if (TravelingDownward)
+            {
+                Y -= Speed;
+            }
+            if(TravelingUpward)
+            {
+                Y += Speed;
+            }
+            if (TravelingLeftward)
+            {
+                X -= Speed;
+            }
+            if (TravelingRightward)
+            {
+                X += Speed;
+            }
+        }
+
+        public void Draw(CanvasDrawingSession canvas)
+        {
+            // when using image, account for x and y being top left
+            canvas.DrawImage(image, X, Y);
+        }
+
+    }
+
+    public interface IDrawable
+    {
+        void Draw(CanvasDrawingSession canvas);
+    }
+
+    public class tankGame
+    {
+        private Tank tank;
+        private List<IDrawable> drawables;
+        private Gamepad controller;
+
+        public tankGame(CanvasBitmap tankimage)
+        {
+            
+
+            drawables = new List<IDrawable>();
+
+            tank = new Tank(100, 100, 5, tankimage);
+
+        }
+
+        public void goingUp(bool TravelingUpward)
+        {
+            tank.TravelingUpward = TravelingUpward;
+        }
+
+        public void goingDown(bool TravelingDownward)
+        {
+            tank.TravelingDownward = TravelingDownward;
+        }
+
+        public void goingLeft(bool TravelingLeftward)
+        {
+            tank.TravelingLeftward = TravelingLeftward;
+        }
+
+        public void goingRight(bool TravelingRightward)
+        {
+            tank.TravelingRightward = TravelingRightward;
+        }
+
+        public void Update()
+        {
+            if (Gamepad.Gamepads.Count > 0)
+            {
+                controller = Gamepad.Gamepads.First();
+                var reading = controller.GetCurrentReading();
+                tank.X += (int)(reading.LeftThumbstickX * 5);
+                tank.Y += (int)(reading.LeftThumbstickY * -5);
+            }
+
+            tank.Update();
+        }
+
+        public void DrawGame(CanvasDrawingSession canvas)
+        {
+            foreach (var drawable in drawables)
+            {
+                drawable.Draw(canvas);
+            }
+        }
+
+
+    }
+
+
+
+}
